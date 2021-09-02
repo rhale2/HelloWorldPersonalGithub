2bac2c3ec8341885b75bf95abda27e7a93f59bd4
[rhale2] <[rhale2@zagmail.gonzaga.edu]>
2021-09-02 11:54:54 -0700

    added!

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     // complied into javabyte
     public static void main (String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello!");
     }
 }
 
